2f1fa86127c506e7bd7b2eec6ed082157b9d98d5
Ivan <ivan.trifunov@outlook.com>
2023-11-03 22:12:56 +0100

    "Changed 1 file"

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,6 +5,7 @@ import com.example.plugins.configureKoin
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
+    // comment
     io.ktor.server.netty.EngineMain.main(args)
 }
 @Suppress("unused")
