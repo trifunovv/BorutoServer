2be17634e2f0e68d9e61e22a0b67d3fee575b31e
Ivan <ivan.trifunov@outlook.com>
2023-11-03 21:26:02 +0100

    "Changed 3 files"

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,9 +5,9 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main() {
+fun main(args: Array<String>) {
     embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
-        // ...
+        EngineMain.main(args)
     }.start(wait = true)
 }
 @Suppress("unused")
